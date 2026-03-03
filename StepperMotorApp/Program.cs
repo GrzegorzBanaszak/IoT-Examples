41150ed69d7e7415524798796aece46eb7430e76
Grzegorz Banaszak <grzegorz.banaszak95@gmail.com>
2026-03-03 11:41:30 +0100

    Add 28BYJ-48 motor test

---
+++ StepperMotorApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using Iot.Device.Uln2003;
+
+Console.WriteLine("--- Test 28BYJ-48: Pełne koło i Reset ---");
+Console.WriteLine("Naciśnij Ctrl+C, aby zatrzymać i wrócić do pozycji 0.");
+
+int pin1 = 17, pin2 = 18, pin3 = 27, pin4 = 22;
+
+// 4096 kroków to pełny obrót (360 stopni) dla silnika 28BYJ-48 w trybie 8-krokowym
+const int FullRotation = 4096;
+int totalStepsMoved = 0;
+bool isRunning = true;
+
+using var motor = new Uln2003(pin1, pin2, pin3, pin4, stepsToRotate: FullRotation);
+
+// Obsługa zdarzenia Ctrl+C
+Console.CancelKeyPress += (s, e) =>
+{
+    e.Cancel = true; // Zapobiegamy natychmiastowemu zamknięciu
+    isRunning = false;
+};
+
+try
+{
+    while (isRunning)
+    {
+        Console.WriteLine("Zataczam pełne koło...");
+
+        // Robimy obrót w małych partiach, aby móc szybciej zareagować na Ctrl+C
+        for (int i = 0; i < FullRotation; i++)
+        {
+            if (!isRunning) break;
+            motor.Step(1);
+            totalStepsMoved++;
+        }
+
+        if (isRunning)
+        {
+            Console.WriteLine("Obrót zakończony. Czekam 2 sekundy...");
+            Thread.Sleep(2000);
+        }
+    }
+}
+finally
+{
+    // RESET DO POZYCJI WYJŚCIOWEJ
+    if (totalStepsMoved != 0)
+    {
+        Console.WriteLine($"\nResetowanie pozycji... Powrót o {-totalStepsMoved} kroków.");
+        // Wykonujemy ruch powrotny
+        motor.Step(-totalStepsMoved);
+        totalStepsMoved = 0;
+    }
+
+    // Wyłączenie cewek (bezpieczeństwo)
+    motor.Step(0);
+    Console.WriteLine("Silnik w pozycji wyjściowej. Koniec.");
+}
---
+++ StepperMotorApp/README.md
@@ -0,0 +1,16 @@
+# 28BYJ-48 Stepper Motor with ULN2003
+
+### Opis
+
+Test silnika krokowego 28BYJ-48 sterowanego przez układ ULN2003 przy użyciu Raspberry Pi i .NET 9.
+
+### Połączenia
+
+| ULN2003 | Raspberry Pi (BCM) |
+| :------ | :----------------- |
+| IN1     | GPIO 17            |
+| IN2     | GPIO 18            |
+| IN3     | GPIO 27            |
+| IN4     | GPIO 22            |
+| VCC     | 5V                 |
+| GND     | GND                |
